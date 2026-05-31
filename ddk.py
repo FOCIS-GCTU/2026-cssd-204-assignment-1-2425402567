24291662565979c60035e1e19c0ff30c09778c42
2425402567 <2425402567@live.gctu.edu.gh>
2026-05-31 07:43:44 +0000

    Add files via upload

---
+++ ddk.py
@@ -0,0 +1,38 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Daniel Darko Kyei
+# Student ID: 2425402567
+
+# Email: 2425402567@live.gctu.edu.gh
+# Grader : Daniel Darko Kyei
+#
+# On my honor , Daniel Darko Kyei, this programming 
+ assignment is  my own work.
+# and I have not provided this code to any  other student.
+
+def main():
+    # This program prints my initials in large letters
+
+    print()
+
+    print("...DDK")
+
+    print()
+
+    print("...DDDDDDDDDD..........DDDDDDDDDD..........KK......KK........")
+    print("...DD........DD........DD........DD........KK....KK..........")
+    print("...DD........DD........DD........DD........KK..KK............")
+    print("...DD........DD........DD........DD........KKKK..............")
+    print("...DD........DD........DD........DD........KKKK..............")
+    print("...DD........DD........DD........DD........KK..KK............")
+    print("...DD........DD........DD........DD........KK....KK..........")
+    print("...DD........DD........DD........DD........KK......KK........")
+    print("...DDDDDDDDDD.....**...DDDDDDDDDD.....**...KK......KK...**...")
+    print("...DDDDDDDDDD.....**...DDDDDDDDDD.....**...KK......KK...**...")
+
+    print()
+
+
+main()
